d33d4a221854ef5a818d55cc41f9c1c44012aa0f
chapa <pavlidis.chrs@gmail.com>
2017-10-15 22:35:56 +0200

    Unneeded files deletion

--- resources/STNs/dis_mnist/read_labels.py
+++
@@ -1,8 +0,0 @@
-import struct
-
-f = open('distorted-t10k-labels.idx1-ubyte','r')
-value = f.read()
-for i in range (100,108):
-	print struct.unpack('B',value[i])[0]
-
-
--- resources/STNs/st_mnist_part/train_lenet.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-./build/tools/caffe train --solver=examples/st_mnist_part/lenet_solver.prototxt
